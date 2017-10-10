ba28f837dde3ff3f16c1d0eb19360f9600fd81f1
aguja <ihernandez@madisoncollege.edu>
2017-10-10 12:04:05 -0500

    searching through the database for a registered user

--- src/main/java/edu/matc/controller/LoginServlet.java
+++ src/main/java/edu/matc/controller/LoginServlet.java
@@ -3,6 +3,7 @@ package edu.matc.controller;
 import edu.matc.entity.UsertableEntity;
 import edu.matc.persistence.GenderHibernateDao;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,11 +21,32 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String userName = request.getParameter("userName");
         String userPassword = request.getParameter("userPassword");
+        String url = null;
+
 
         GenderHibernateDao listOfUser = new GenderHibernateDao();
         List<UsertableEntity> userList = new ArrayList<>();
         userList = listOfUser.getUserByNameAndPassword(userName, userPassword);
 
+        if (userList.size() == 1){
+            url = "jsp/chooseSection.jsp";
+            // Redirect the flow
+            response.sendRedirect(url);
+        } else {
+            //url = "HomePageServlet";
+            doGet(request, response);
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+        String url = "/jsp/login.jsp";
+
+        RequestDispatcher dispatcher =
+                getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
     }
 
 }
---
+++ src/main/webapp/jsp/chooseSection.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: aguja001
+  Date: 10/10/2017
+  Time: 11:24 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Choose a section</title>
+</head>
+<body>
+    <h2>The user has been logged in</h2>
+</body>
+</html>
